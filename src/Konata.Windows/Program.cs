f2ee50877a16fbe6ee7da2c7852db0ededae079e
Tomat <xxlennygamerxx@gmail.com>
2022-07-06 00:32:47 -0700

    Working bootstrapper.

---
+++ src/Konata.Windows/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Konata.Windows
+{
+    public static class Program
+    {
+        // 0 = runner path
+        // 1 = data path
+        // 2 = yytk path
+        public static void Main(string[] args) {
+            if (!OperatingSystem.IsWindows()) throw new PlatformNotSupportedException("Konata.Windows exclusively supports Windows.");
+
+            if (args.Length != 3) throw new ArgumentException("Three arguments not provided.");
+
+            foreach (string path in args) {
+                if (!File.Exists(path)) {
+                    throw new FileNotFoundException($"File does not exist at: {path}");
+                }
+            }
+
+            DllInject.StartInjected(args[0], $"-game \"{args[1]}\"", args[2]);
+        }
+    }
+}
---
+++ src/Konata.Windows/WindowsApiMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace Konata.Windows
+{
+    // Adapted from https://github.com/Archie-osu/YYToolkit/blob/stable/YYLauncher/WinAPI.cs
+    [SupportedOSPlatform("windows")]
+    internal static class WindowsApiMethods
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            ref WindowsApiTypes.SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref WindowsApiTypes.SECURITY_ATTRIBUTES lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref WindowsApiTypes.STARTUPINFO lpStartupInfo,
+            out WindowsApiTypes.PROCESS_INFORMATION lpProcessInformation
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint ResumeThread(IntPtr hThread);
+        
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(
+            IntPtr hModule,
+            string procName
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(
+            string lpModuleName
+        );
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            uint dwSize,
+            uint flAllocationType,
+            uint flProtect
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            uint nSize,
+            out UIntPtr lpNumberOfBytesWritten
+        );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(
+            IntPtr hProcess,
+            IntPtr lpThreadAttributes,
+            uint dwStackSize,
+            IntPtr lpStartAddress,
+            IntPtr lpParameter,
+            uint dwCreationFlags,
+            IntPtr lpThreadId
+        );
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetProcessId(
+            IntPtr handle
+        );
+    }
+}
